b9e486c29cf26d6d9ac2b73b699436e2896e1a65
Federico Tensi <federicotensi@gmail.com>
2022-10-29 21:17:49 +0200

    fixed some css error, added core funtionality

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,13 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+
+test('renders the app', async () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+
+  // await screen.findByTestId('container');
+
+  expect(await screen.findByText('Author:')).toBeInTheDocument();
+
 });
+
